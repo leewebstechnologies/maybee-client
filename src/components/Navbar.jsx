c829bb4e0e3b76ff4645f64e74b53c08f51b7aca
leewebstechnologies <aliuajibade317@gmail.com>
2023-07-09 23:36:20 +0100

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 60px;
+  background-color: #000;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <Container>Navbar</Container>;
 };
 
 export default Navbar;
