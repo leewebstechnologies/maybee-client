71b64a8b4eeec5bd4f14573342e2fdfc82910581
leewebstechnologies <aliuajibade317@gmail.com>
2023-07-10 23:10:53 +0100

    material ui added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,54 @@
+import { Search } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: #000;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
 `;
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+          
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
